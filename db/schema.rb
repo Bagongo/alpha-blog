48b5b27d711defb301831553c2140bb2365c8a1f
Ale <akak@hotmail.it>
2017-06-21 15:15:20 +0200

    Added user-article association and debugger rendering....

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620142345) do
+ActiveRecord::Schema.define(version: 20170621104009) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
